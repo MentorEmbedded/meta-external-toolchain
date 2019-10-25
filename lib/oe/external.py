14fa5f807929feaf1f2aca474afabd0ef002169b
Christopher Larson <chris_larson@mentor.com>
2019-10-25 21:03:01 +0500

    oe.external.run: let cmd already start with the prefix
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

--- lib/oe/external.py
+++ lib/oe/external.py
@@ -10,9 +10,12 @@ def run(d, cmd, *args):
     topdir = d.getVar('TOPDIR')
     toolchain_path = d.getVar('EXTERNAL_TOOLCHAIN')
     if toolchain_path:
-        target_cmd = d.getVar('EXTERNAL_TARGET_SYS') + '-' + cmd
+        target_prefix = d.getVar('EXTERNAL_TARGET_SYS') + '-'
+        if not cmd.startswith(target_prefix):
+            cmd = target_prefix + cmd
+
         toolchain_bin = d.getVar('EXTERNAL_TOOLCHAIN_BIN')
-        path = os.path.join(toolchain_bin, target_cmd)
+        path = os.path.join(toolchain_bin, cmd)
         args = shlex.split(path) + list(args)
 
         try:
